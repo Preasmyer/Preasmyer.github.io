bfd1c5a82f52d17cc2a26e3cdcf58b11531a3d93
Cameron Preasmyer <spc6uz@virginia.edu>
2025-06-17 17:40:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+let pdfDoc = null,
+    pageNum = 1,
+    canvas = document.getElementById("pdf-canvas"),
+    ctx = canvas.getContext("2d");
+
+pdfjsLib.getDocument("assets/my_project.pdf").promise.then(doc => {
+    pdfDoc = doc;
+    renderPage(pageNum);
+});
+
+function renderPage(num) {
+    pdfDoc.getPage(num).then(page => {
+        let viewport = page.getViewport({ scale: 1.5 });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        let renderContext = { canvasContext: ctx, viewport: viewport };
+        page.render(renderContext);
+    });
+}
+
+function nextPage() {
+    if (pageNum < pdfDoc.numPages) {
+        pageNum++;
+        renderPage(pageNum);
+    }
+}
+
+function prevPage() {
+    if (pageNum > 1) {
+        pageNum--;
+        renderPage(pageNum);
+    }
+}
 
