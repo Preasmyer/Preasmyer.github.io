5e90cbaf5b1bd598a0838d825a0d0b7192fe925f
Cameron Preasmyer <spc6uz@virginia.edu>
2025-06-16 21:29:26 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const projects = document.querySelectorAll(".project a");
+    projects.forEach(link => {
+        link.addEventListener("mouseover", () => {
+            link.style.transform = "scale(1.1)";
+        });
+        link.addEventListener("mouseleave", () => {
+            link.style.transform = "scale(1)";
+        });
+    });
+});
 
